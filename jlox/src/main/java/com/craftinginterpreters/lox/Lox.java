809c1cc53c7ef1a157435fc7913c7a2091dd8d89
Sravan Patibandla <pbskumar@live.com>
2025-12-11 23:33:37 -0800

    Adds Scanner class to convert source code to tokens

--- jlox/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -1,24 +1,27 @@
 package com.craftinginterpreters.lox;
 
+import com.craftinginterpreters.lox.scanner.Scanner;
+import com.craftinginterpreters.lox.token.Token;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Lox {
 
     private static boolean hadError = false;
 
     public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
-            System.exit(64);
-        } else if (args.length == 1) {
-            runFile(args[0]);
-        } else {
-            runPrompt();
+        switch (args.length) {
+            case 0: runPrompt(); break;
+            case 1: runFile(args[0]); break;
+            default:
+                System.out.println("Usage: jlox [script]");
+                System.exit(64);
         }
     }
 
@@ -43,12 +46,23 @@ public class Lox {
         }
     }
 
+    // Converts to a list of tokens
+    // Next step is make sense of the tokens.
     private static void run(final String source) {
-        System.out.println("lox: " + source);
+        final Scanner scanner = new Scanner(source);
+        final List<Token> tokens = scanner.scanTokens();
+
+        for (final Token token : tokens) {
+            System.out.println("Token: " + token);
+        }
     }
 
-    static void error(final int line, final String message) {
+    public static void error(final int line, final String message) {
         report(line, "", message);
+    }
+
+    public static void error(final int line, final String where, final String message) {
+        report(line, where, message);
     }
 
     private static void report(final int line, final String where, final String message) {
--- jlox/src/main/java/com/craftinginterpreters/lox/token/Token.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/token/Token.java
@@ -3,7 +3,9 @@ package com.craftinginterpreters.lox.token;
 // TODO: Add column number
 public record Token (
         TokenType type,
+        // Text from source identified as part of the Token
         String lexeme,
+        // Value of the token
         Object literal,
         int line
 ) {
