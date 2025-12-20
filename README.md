cd7c35bdfdd45739eb9bbd9d5ae88f72f09cd23c
Sravan Patibandla <pbskumar@live.com>
2025-12-19 22:54:53 -0800

    Add AST generator class

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ This repo is an implementation of `lox` language based on [Crafting Interpreters
 
 
 ## Why?
-I built the SQL parsing and transpiling library for [IoT SiteWise Query Language](https://docs.aws.amazon.com/iot-sitewise/latest/userguide/sql.html). I never expected to enjoy working at the nitty-gritty level of AST traversals and optimizations, but it turned out to be a lot of fun. Coming from a non-CS background, I’ve always been fascinated by the “magic” behind languages and their interpreters or compilers. Now that I’ve gotten my hands dirty with lexing, parsing, AST traversal, and transpiling (all thanks to Visitor Pattern!), I want to go all the way—build a language from scratch and really understand what happens under the hood of interpreters. And what better way than college class styled book!
+When building the SQL parsing and transpiling library for [IoT SiteWise Query Language](https://docs.aws.amazon.com/iot-sitewise/latest/userguide/sql.html), 
+I never expected to enjoy working at the nitty-gritty level of AST traversals and optimizations, 
+but it turned out to be a lot of fun. Coming from a non-CS background, I’ve always been fascinated 
+by the “magic” behind languages and their interpreters or compilers. 
+
+Now that I’ve gotten my 
+hands dirty with lexing, parsing, AST traversal, and transpiling (all thanks to Visitor Pattern!), 
+I want to go all the way—build a language from scratch and really understand what happens under the hood 
+of interpreters. 
+
+And what better way than through this fun book!
 
 ## jlox - Java based interpreter for lox
 
@@ -18,7 +28,7 @@ I built the SQL parsing and transpiling library for [IoT SiteWise Query Language
 
 3. Add `jlox` to your environment temporarily
 
-   You could run `jlox` using the long format like `./gradlew run` or `build/install/jlox/bin/jlox <fname>`. 
+   You could run `jlox` using the long format like `build/install/jlox/bin/jlox <fname>`. 
 Second option is possible because we configured Gradle to create a distribution. 
 But, it is much easier to have the distribution in our path. 
    
---
+++ jlox/src/tool/java/com.craftinginterpreters.tool/GenerateAst.java
@@ -0,0 +1,115 @@
+package com.craftinginterpreters.tool;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+
+        final String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary     : Expr left, Token operator, Expr right",
+                "Grouping   : Expr expression",
+                "Literal    : Object value",
+                "Unary      : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(final String outputDir,
+                                  final String baseName,
+                                  final List<String> types) throws IOException {
+
+        Path dir = Paths.get(outputDir, "com", "craftinginterpreters", "lox", "ast");
+        Files.createDirectories(dir); // idempotent
+
+        Path file = dir.resolve(baseName + ".java");
+
+        try (final BufferedWriter writer = Files.newBufferedWriter(file, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+
+            writer.write("package com.craftinginterpreters.lox.ast;");
+            writer.newLine(); writer.newLine();
+
+            writer.write("import com.craftinginterpreters.lox.token.Token;");
+            writer.newLine(); writer.newLine();
+
+            writer.write("import java.util.List;");
+            writer.newLine(); writer.newLine();
+
+            writer.write("abstract class %s {".formatted(baseName));
+            writer.newLine();
+
+            for (final String type : types) {
+                final List<String> grammar = Stream.of(type.split(":")).map(String::trim).toList();
+                if (grammar.size() != 2) continue;
+                // Dirty way to get the strings
+                final String className = grammar.getFirst().trim();
+                final String fields = grammar.getLast().trim();
+                defineType(writer, baseName, className, fields);
+            }
+
+            writer.write("}");
+            writer.newLine();
+        }
+
+    }
+
+    private static void defineType(final BufferedWriter writer,
+                                   final String baseName,
+                                   final String className,
+                                   final String fieldList) throws IOException {
+
+        writer.newLine();
+        writer.write("\tstatic class %s extends %s {".formatted(className, baseName));
+        writer.newLine();
+
+        final LinkedHashMap<String, String> fields = Arrays.stream(
+                fieldList.split(","))
+                .map(String::strip)
+                .map(f -> f.split("\\s+"))
+                .collect(Collectors.toMap(
+                        parts -> parts[0].strip(),
+                        parts -> parts[1].strip(),
+                        (a, b) -> a,
+                        LinkedHashMap::new
+                ));
+
+        // Fields
+        for (final Map.Entry<String, String> entry: fields.sequencedEntrySet()) {
+            writer.write("\t\tfinal %s %s;".formatted(entry.getKey(), entry.getValue()));
+            writer.newLine();
+        }
+        writer.newLine();
+
+        // Constructor
+        final String constructorArgs = fields.sequencedEntrySet()
+                .stream().map((entry) -> "final %s %s".formatted(entry.getKey(), entry.getValue()))
+                .collect(Collectors.joining(", "));
+        writer.write("\t\t%s(%s) {".formatted(className, constructorArgs));
+        writer.newLine();
+
+        for (final Map.Entry<String, String> entry: fields.sequencedEntrySet()) {
+            writer.write("\t\t\tthis.%s = %s;".formatted(entry.getValue(), entry.getValue()));
+            writer.newLine();
+        }
+        writer.write("\t\t}");
+        writer.newLine();
+
+        writer.write("\t}");
+        writer.newLine();
+    }
+}
